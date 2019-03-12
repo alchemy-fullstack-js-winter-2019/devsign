e320c63e9d303dc158f78bff866e371cc43a8cea
Kate Dameron <katerj33@gmail.com>
2019-03-11 22:49:44 -0700

    dammit travis

--- src/components/Signup.test.js
+++ src/components/Signup.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Signup from './Signup';
+import Signup from './Signup.js';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 
