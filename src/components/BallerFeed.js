9e003221b97641294397d3ef0d2714dc6230a39e
Ivan <i.poblete915@gmail.com>
2019-03-11 20:56:32 -0700

    changed image sixes on feed

--- src/components/BallerFeed.js
+++ src/components/BallerFeed.js
@@ -6,17 +6,17 @@ function BallerFeed() {
       <h2>Ball is Life</h2>
       <h3>Jackmerius Tacktheritrix</h3>
       <p>Ball everyday!</p>
-      <img alt="placeholder" src="https://via.placeholder.com/150" />
+      <img alt="placeholder" src="https://via.placeholder.com/300" />
     
       <h2>Trade Alert!</h2>
       <h3>Hingle McCringleberry</h3>
       <p>Player traded!</p>
-      <img alt="placeholder" src="https://via.placeholder.com/150" />
+      <img alt="placeholder" src="https://via.placeholder.com/300" />
     
       <h2>Baller Alert!</h2>
       <h3>Davoin Shower-Handel</h3>
       <p>Player injured!</p>
-      <img alt="placeholder" src="https://via.placeholder.com/150" />
+      <img alt="placeholder" src="https://via.placeholder.com/300" />
     </section>
   );
 }
