3af373d1394a62828e0e4b9b4d055e2138f37d13
Ivan <i.poblete915@gmail.com>
2019-03-11 20:43:26 -0700

    wired up Header and Footer

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>HELLO WORLD</h1>
+      <Header />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <h2>Hello Footer</h2>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/search">Search</a></li>
+        </ul>
+      </nav>  
+    </footer>
+  );
+}
+
+export default Footer;
