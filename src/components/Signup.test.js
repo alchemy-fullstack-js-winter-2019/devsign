f212f16e6385b27e662c91db3288ecfa33a4ce7f
Kate Dameron <katerj33@gmail.com>
2019-03-11 23:47:33 -0700

    ugh

--- src/components/Signup.test.js
+++ src/components/Signup.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Signup from './Signup.js';
+import Signup from './Signup';
 import renderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
 
 describe('Signup component', () => {
   it('matches a snapshot', () => {
     const tree = renderer.create(
-      <MemoryRouter>
-        <Signup />
-      </MemoryRouter>
+      <Signup />
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
