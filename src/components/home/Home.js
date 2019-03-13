3591ba8e6240cde05b51d45a25fd4ac75af330d0
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-13 14:08:42 -0700

    setting up store

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,15 +2,6 @@ import React from 'react';
 import style from './Home.css';
 import Tweets from '../tweets/Tweets';
 
-const tweets = 
-  [
-    { _id: '1', text: 'my first tweet', user: { handle: 'Larry_Bird', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '2', text: 'my second tweet', user: { handle: 'Big_Bird', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '3', text: 'my third tweet', user: { handle: 'Tweety', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '4', text: 'my fourth tweet', user: { handle: 'Scrooge', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '5', text: 'my firth tweet', user: { handle: 'R_R', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } }
-  ];
-
 export default function Home() {
   return (
     <main className={style.main}>
