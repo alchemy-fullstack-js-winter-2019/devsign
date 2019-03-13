55a6831f27a5ddac1e1d7bd0f39d1a4b6a173c7c
Shaba9 <shaba92@gmail.com>
2019-03-13 09:20:31 -0700

    mock data displaying for all pages

--- src/containers/tweets/TopTweets.js
+++ src/containers/tweets/TopTweets.js
@@ -8,10 +8,33 @@ export default class TopTweets extends PureComponent {
   }
 
   render() {
+    const tweets = [
+      {
+        id: 1,
+        userId: 1,
+        img: 'https://cdn.shopify.com/s/files/1/1061/1924/products/Thinking_Face_Emoji_large.png?v=1480481060',
+        text: 'twitter you twit'
+      },
+      {
+        id: 2,
+        userId: 2,
+        img: 'https://cdn.shopify.com/s/files/1/1061/1924/products/Thinking_Face_Emoji_large.png?v=1480481060',
+        text: 'twitter you twit twit'
+      },
+      {
+        id: 3,
+        userId: 3,
+        img: 'https://cdn.shopify.com/s/files/1/1061/1924/products/Thinking_Face_Emoji_large.png?v=1480481060',
+        text: 'twitter you twit twit twitter twit'
+      }
+    ];
+    
+    const props = { tweets };
+
     return (
       <main>
         <h2>Top Tweets:</h2>
-        <Tweets {...this.props} />
+        <Tweets {...props} />
       </main>
     );
   }
