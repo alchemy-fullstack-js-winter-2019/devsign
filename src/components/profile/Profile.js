97564c365c0465e8e6adead199856c6506588233
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-12 20:17:54 -0700

    user/profile css

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tweets from '../../components/tweets/Tweets';
-import styles from './Profile.css';
+import User from '../user/User';
+
 const tweets = 
   [
     { _id: '1', text: 'my first tweet', user: { handle: 'T_on_A', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
@@ -9,18 +10,17 @@ const tweets =
     { _id: '4', text: 'my fourth tweet', user: { handle: 'T_on_A', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
     { _id: '5', text: 'my firth tweet', user: { handle: 'T_on_A', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } }
   ];
+const user = {
+  handle: 'T_on_A',
+  name: 'Teonna Zaragoza',
+  image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg',
+  bio: 'I am an avid trail runner, nurse and software developer'
+};
 
 export default function Profile() {
   return (
     <main>
-      <article styles={styles}>
-        <figure>
-          <img src="https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg" alt="user avatar" />
-        </figure>
-        <h3>Teonna</h3>
-        <h4>T_on_A</h4>
-        <p>Bio about me</p>
-      </article>
+      <User user={user}/>
       <Tweets tweets={tweets}/>
       <aside>
         <p>Trends</p>
