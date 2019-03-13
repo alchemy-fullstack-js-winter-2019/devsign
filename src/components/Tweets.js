6505d7a6f7f48e71ae69c795c83faf7c7dbca239
Carmen Perezchica <crperezchica@gmail.com>
2019-03-13 09:33:35 -0700

    no data, commented out .map form tweets file

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Tweet from './Tweet';
+// import Tweet from './Tweet';
 
 
-export default function Tweets({ tweets }) {
-  const listOfTweets = tweets.map((tweet, i) => {
-    return <li key={i}><Tweet profileImage={tweet.profileImage} name={tweet.name} text={tweet.text} handle={tweet.handle} /></li>;
-  });
+export default function Tweets() {
+  // const listOfTweets = tweets.map((tweet, i) => {
+  //   return <li key={i}><Tweet profileImage={tweet.profileImage} name={tweet.name} text={tweet.text} handle={tweet.handle} /></li>;
+  // });
 
   return (
     <section >
       <ul>
-        <li>{listOfTweets}</li>
+        <li>tweets</li>
       </ul>
     </section>
   );
