b08ed05e392eeec829da293d3233f1141adee224
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-13 08:29:52 -0700

    fixed snapshots

--- src/components/Tweet.test.js
+++ src/components/Tweet.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import CreateTweet from './CreateTweet';
+import Tweet from './Tweet';
 import { MemoryRouter } from 'react-router-dom';
 
 let tweet = { body: 'Best tweet', id: 21, likes: 40, retweets: 5 };
@@ -9,7 +9,7 @@ describe('CreateTweet', () => {
   it('renders CreateTweet component', () => {
     const tree = renderer.create(
       <MemoryRouter>
-        <CreateTweet tweet={tweet}/>
+        <Tweet tweet={tweet}/>
       </MemoryRouter>
     );
     expect(tree).toMatchSnapshot;
