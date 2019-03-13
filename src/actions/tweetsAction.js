c519d553316f336ce5cedf52c8787413ae5adc90
Carmen Perezchica <crperezchica@gmail.com>
2019-03-13 14:24:49 -0700

    created server to get tweets, reducer, action, selector, container to get tweet and created store

---
+++ src/actions/tweetsAction.js
@@ -0,0 +1,14 @@
+import { getTweets } from '../services/tweets';
+
+export const FETCH_TWEETS = 'FETCH_TWEETS';
+export const fetchTweets = () => ({
+  type: FETCH_TWEETS,
+  // pendingType: FETCH_TWEETS_PENDING,
+  payload: getTweets
+});
+
+// export const [
+//   fetchTweets,
+//   FETCH_TWEETS,
+//   FETCH_TWEETS_PENDING
+// ] = createAction('FETCH_TWEETS', getTweets);
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../Home';
+import Landing from '../Landing';
 import Registration from '../Registration';
 import Tweet from '../Tweet';
+import Home from '../Home';
 import 'normalize.css';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Registration} />
         <Route exact path="/tweet" component={Tweet} />
+        <Route exact paht="/Home" component={Home} />
       </Switch>
     </Router>
   );
