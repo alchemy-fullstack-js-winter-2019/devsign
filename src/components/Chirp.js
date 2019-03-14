9c1de817afbfffb58cd8b8a70d21cc6a511f71ea
Kate Dameron <katerj33@gmail.com>
2019-03-13 21:05:22 -0700

    chirp styling

--- src/components/Chirp.js
+++ src/components/Chirp.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ProfileImg, Handle, Text, Article } from '../styles/mainStyle';
+import styled from 'styled-components';
+
+const HTwo = styled.h2 `
+  margin: 0;
+`;
+
+const P = styled(Text) `
+  margin: 0;
+  padding: 0;
+  padding-left: 13%;
+`;
 
 function Chirp({ name, handle, text, profileImage }) {
   return (
     <Article>
       <header>
         <ProfileImg alt={name} src={profileImage}/>
-        <h2>{name} <Handle>{handle}</Handle></h2>
+        <HTwo>{name} <Handle>{handle}</Handle></HTwo>
       </header>
-      <Text>{text}</Text>
+      <P>{text}</P>
     </Article>
   );
 }
