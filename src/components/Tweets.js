c145648d345cb8bc360a0537a58cbb41be24adbf
Carmen Perezchica <crperezchica@gmail.com>
2019-03-14 09:56:44 -0700

    fixing to pass travis

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // import Tweet from './Tweet';
 
 
@@ -17,6 +17,6 @@ export default function Tweets() {
   );
 }
 
-Tweets.propTypes = {
-  tweets: PropTypes.array.isRequired
-};
+// Tweets.propTypes = {
+//   tweets: PropTypes.array.isRequired
+// };
