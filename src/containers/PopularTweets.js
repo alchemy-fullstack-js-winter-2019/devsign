c3c95add4c7d4bce119716b53f0f04d6d7b95d6f
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-13 17:22:59 -0700

    popular tweets for landing page

---
+++ src/containers/PopularTweets.js
@@ -0,0 +1,35 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import Tweets from '../components/tweets/Tweets';
+import PropTypes from 'prop-types';
+import { getTweets } from '../selectors/tweets';
+import { fetchTweets } from '../actions/tweets';
+
+class PopularTweets extends PureComponent {
+  static propTypes = {
+    tweets: PropTypes.array,
+    // fetch: PropTypes.function.isRequired
+  }
+  componentDidMount() {
+    this.props.fetch();
+  }
+  render() {
+    return (
+      <Tweets {...this.props}/>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  tweets: getTweets(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    return dispatch(fetchTweets());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PopularTweets);
