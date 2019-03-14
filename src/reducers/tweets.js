e9573b7e54bb833b157719c394140cbb6db2b85f
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-13 17:23:15 -0700

    tweets reducer and selector

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,7 +1,16 @@
+import { FETCH_TWEETS } from '../actions/tweets';
 const initialState = {
   tweets: []
 };
 
 export default function reducer(state = initialState, action) {
-  return state;
+  switch(action.type) {
+    case FETCH_TWEETS:
+      return {
+        ...state,
+        tweets: action.payload
+      };
+    default:
+      return state;
+  }
 }
