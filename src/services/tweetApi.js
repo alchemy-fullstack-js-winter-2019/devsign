a2a91417ea58628bb5490aee3423b011a9c87ebb
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-15 14:10:54 -0700

    linking to heroku app

--- src/services/tweetApi.js
+++ src/services/tweetApi.js
@@ -1,8 +1,6 @@
 export const getAllTweets = () =>  {
-    return Promise.resolve([
-        { handle: 'kananiBoy', img: 'https://via.placeholder.com/60', body: 'some body', id: 1, likes: 100, retweets: 10000 },
-        { handle: 'kananiBoy', img: 'https://via.placeholder.com/60', body: 'some body', id: 2, likes: 100, retweets: 10000 }, { handle: 'kananiBoy', img: 'https://via.placeholder.com/60', body: 'some body', id: 3, likes: 100, retweets: 10000 },
-    ]);
+    return fetch('https://git.heroku.com/twitterclonebe.git/tweets')
+        .then(res => res.json());
 }
 ;
 
