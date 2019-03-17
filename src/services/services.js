94d85d520a7dea74365c4dc720bc5f9cfc6183f2
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-16 18:08:50 -0700

    changed services

--- src/services/services.js
+++ src/services/services.js
@@ -1,11 +1,17 @@
 export const getTweets = () => {
-  return Promise.resolve([
-    { _id: '1', text: 'my first tweet', user: { handle: 'Larry_Bird', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '2', text: 'my second tweet', user: { handle: 'Big_Bird', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '3', text: 'my third tweet', user: { handle: 'Tweety', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '4', text: 'my fourth tweet', user: { handle: 'Scrooge', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
-    { _id: '5', text: 'my firth tweet', user: { handle: 'R_R', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } }
-  ]);
+  return fetch(`${process.env.API_URL}/tweets`)
+    .then(res => [res.ok, res.json()])
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json; 
+    });
+  // return Promise.resolve([
+  //   { _id: '1', text: 'my first tweet', user: { handle: 'Larry_Bird', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
+  //   { _id: '2', text: 'my second tweet', user: { handle: 'Big_Bird', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
+  //   { _id: '3', text: 'my third tweet', user: { handle: 'Tweety', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
+  //   { _id: '4', text: 'my fourth tweet', user: { handle: 'Scrooge', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } },
+  //   { _id: '5', text: 'my firth tweet', user: { handle: 'R_R', image: 'https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg' } }
+  // ]);
 };
 
 export const getMyTweets = () => {
