fd72c53845ca8f69db771f949b23056643b06c30
Carmen Perezchica <crperezchica@gmail.com>
2019-03-17 17:10:42 -0700

    created an effect on submit button on create tweet form

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './css/Loading.css';
+
+export default function Loading() {
+  return (
+    <section className={styles.loading}>
+
+    </section>
+  );
+}
