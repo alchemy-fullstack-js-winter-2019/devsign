b0c80d165340535d91026d64d1ceb66efe9a8f40
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-22 14:14:43 -0700

    More CSS and rerun snapshot tests

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,14 @@ export default function About() {
   return (
     <>
       <Header/>
-      <section>
+      <section  id="carmen"className={styles.About}>
         <h3>About CARMEN</h3>
-        <div className={styles.About}>
-          <p>Mother,  student,  software developer, food and nutrition enthusiast.</p>
-          <p>My philosophy lines up with that of Hippocrates, the Father of Medicine: Let food be thy medicine and medicine your food. </p>
-          <p>I love food! I enjoy drying fruits and vegetables (kale chips are my favorite) and making health bars from raw nuts, seeds, cacao powder and dates.</p>
+        <div>
+          <p> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"/>Mother,  student,  software developer, food and nutrition enthusiast.</p>
+          <p> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"/>Health philosophy lines up with Hippocrates: Let food be thy medicine and medicine your food. </p>
+          <p> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"/>Loves and passionate about REAL food! </p>
+          <p> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"/>Enjoys drying fruits and vegetables (kale chips are her favorite)!</p>   
+          <p> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"/>Concocts health bars from raw nuts, seeds, cacao powder and dates.</p>
         </div>
       </section>
       <Footer/>
