3c1030f3b9411ffab562e7de2ac2c17cbffb96b1
Ivan <i.poblete915@gmail.com>
2019-03-24 21:35:59 -0700

    still working on form

--- src/components/quibs/QuibForm.js
+++ src/components/quibs/QuibForm.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../css/QuibForm.css';
 
-function QuibForm({ text, onSubmit, onChange }) {
+function QuibForm({ body, handleSubmit, onChange }) {
   return (
-    <form onSubmit={onSubmit}>
-      <textarea name="text" value={text} onChange={onChange} />
+    <form className={styles.form} onSubmit={handleSubmit.bind(null, body)}>
+      <textarea type="text"name="body" value={body} onChange={onChange} />
       <button>Add Quib</button>
     </form>
   );
 }
 
 QuibForm.propTypes = {
-  text: PropTypes.string.isRequired,
-  onSubmit: PropTypes.func,
+  body: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func,
   onChange: PropTypes.func
 };
 
